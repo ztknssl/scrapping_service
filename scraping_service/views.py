0418133be608c94ad63a5574255e92f7cf158aeb
ztknssl <davydov.andrey37@gmail.com>
2020-12-04 12:43:27 +0300

    Second commit

--- scraping_service/views.py
+++ scraping_service/views.py
@@ -6,4 +6,4 @@ def home(request):
     date = datetime.datetime.now().date()
     name = 'Dave'
     _context = {'date': date, 'name': name}
-    return render(request, 'home.html', _context)
+    return render(request, 'base.html', _context)
